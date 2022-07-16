353d339c33bad6eb107b664843fdedd26c2d3632
Yan <picturepan2@hotmail.com>
2022-07-17 03:19:01 +0800

    Update apollo.js

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-    uri: process.env.GRAPHQL_SERVER,
+    uri: "https://relation-service.nextnext.id/",
     cache: new InMemoryCache(),
 });
 
