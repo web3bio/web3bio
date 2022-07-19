82ea26e85f6f3c778bac41cfd94d885ae70f9c54
Yan <picturepan2@hotmail.com>
2022-07-19 22:36:56 +0800

    Update

--- components/search.js
+++ components/search.js
@@ -1,26 +1,39 @@
-import React from 'react'
-import { gql, useQuery } from '@apollo/client'
-import { GET_PROFILES_ENS } from '../utils/queries'
+import { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import { GET_PROFILES_ENS, GET_PROFILES_ETH, GET_PROFILES_TWITTER } from '../utils/queries'
 
 export default function Search() {
-  const { loading, error, data } = useQuery(GET_PROFILES_ENS, {
-    variables: { ens: 'nykma.eth' },
-  })
+  const [searchterm, setSearchterm] = useState('')
+  const [searchResult, setSearchResult] = useState()
+  const [searchFocus, setSearchFocus] = useState(false)
 
+  const handleFocus = () => {
+    setSearchFocus(true)
+    console.log(searchFocus)
+  }
+
+  const { loading, error, data } = useQuery(GET_PROFILES_ENS, {
+    variables: { ens: searchterm },
+  })
   console.log(data)
 
+  const handleSubmit = (e) => {
+    setSearchterm(e.target.value)
+    console.log(e.target.value)
+  }
+
   return (
-    <>
-      <form autoComplete="off">
-        {/* {loading? <div className='loading'></div> : null} */}
+    <div className={searchFocus?'focused':'inactive'}>
+      <form autoComplete="off" role="search">
         <input
           type="text"
           placeholder="Search Twitter, ENS or Ethereum address"
           className='form-input input-lg'
+          id='searchterm'
+          onFocus={handleFocus}
         />
+        <input className="d-none" type="submit" value="Submit"></input>
       </form>
-      {data?.nft.owner.displayName}
-      <div></div>
-    </>
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import Search from '../components/search'
 
 export default function Home() {
-  
+  const [searchterm, setSearchterm] = useState('')
+  const [searchResult, setSearchResult] = useState()
+  const [searchFocus, setSearchFocus] = useState(false)
+
+  const handleFocus = () => {
+    setSearchFocus(true)
+    console.log(searchFocus)
+  }
+
   return (
     <div>
       <Head>
@@ -12,10 +21,36 @@ export default function Home() {
       </Head>
 
       <main className='web3bio-container'>
+        <div className="web3bio-header">
+          <div className="container grid-lg">
+            <div className="columns">
+              <div className="column col-12">
+                <a href="/" className="web3bio-logo" title="Web3.bio">
+                  <h1>WEB3<br/>BIO</h1>
+                </a>
+                <div className="web3bio-account">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="web3bio-cover creamwhisper"></div>
-        <div className="web3bio-hero">
-          <div className="container grid-sm">
-            <Search />
+
+        <div className={searchFocus?'web3bio-search focused':'web3bio-search'}>
+          <div className="container grid-xs">
+            <form className='web3bio-form' autoComplete="off" role="search">
+              <label className='form-label' for="searchbox"><span>Web3 Identity Search</span></label>
+              <input
+                type="text"
+                placeholder="Search Twitter, ENS or Ethereum address"
+                className='form-input input-lg'
+                id='searchbox'
+                onFocus={handleFocus}
+              />
+              <input className="d-none" type="submit" value="Submit"></input>
+            </form>
           </div>
         </div>
       </main>
