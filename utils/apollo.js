17f15faf29e28d63a214f70b57f5c223f1754059
Yan <picturepan2@hotmail.com>
2022-07-20 23:53:22 +0800

    Update env

--- utils/apollo.js
+++ utils/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-    uri: 'https://relation-service.nextnext.id/',
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER,
     cache: new InMemoryCache(),
 });
 
--- utils/queries.js
+++ utils/queries.js
@@ -3,8 +3,6 @@ import { gql } from '@apollo/client'
 export const GET_PROFILES_ENS = gql`
   query($ens: String) {
     nft(chain: "ethereum", category: "ENS", id: $ens) {
-      uuid
-      updatedAt
       owner {
         uuid
         platform
