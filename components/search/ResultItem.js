59175be06f5d5f24708644f203525870e1a9e663
Yan <picturepan2@hotmail.com>
2022-08-02 10:30:39 +0800

    Update ENS lists

--- components/search/ResultItem.js
+++ components/search/ResultItem.js
@@ -13,40 +13,47 @@ class ResultItem extends Component {
     switch (identity.platform) {
       case 'ethereum':
         return (
-          <>
+          <div className="social-item">
             <a className="social ethereum" href={`https://etherscan.io/address/${identity.identity}`} target="_blank" rel="noopener noreferrer">
               <SVG src="icons/icon-ethereum.svg" className="icon" />
               {identity.displayName ? identity.displayName:identity.identity}
             </a>
-            {identity.nft ? (
+            {identity.nft.length > 0 ? (
               <div className="search-result-body">
                 {identity.nft.map((nft) => (
-                  <div key={nft.uuid}>{nft.id}</div>
+                  <span className="label mr-1 mb-1" key={nft.uuid}>{nft.id}</span>
                 ))}
               </div>
             ): null}
-          </>
+          </div>
         )
       case 'twitter':
         return (
-          <a className="social twitter" href={`https://twitter.com/${identity.identity}`} target="_blank" rel="noopener noreferrer">
-            <SVG src="icons/icon-twitter.svg" className="icon" />
-            {identity.displayName}
-          </a>
+          <div className="social-item">
+            <a className="social twitter" href={`https://twitter.com/${identity.identity}`} target="_blank" rel="noopener noreferrer">
+              <SVG src="icons/icon-twitter.svg" className="icon" />
+              {identity.displayName}
+            </a>
+          </div>
         )
       case 'github':
         return (
-          <a className="social github" href={`https://github.com/${identity.identity}`} target="_blank" rel="noopener noreferrer">
-            <SVG src="icons/icon-github.svg" className="icon" />
-            {identity.displayName}
-          </a>
+          <div className="social-item">
+            <a className="social github" href={`https://github.com/${identity.identity}`} target="_blank" rel="noopener noreferrer">
+              <SVG src="icons/icon-github.svg" className="icon" />
+              {identity.displayName}
+            </a>
+          </div>
         )
       case 'keybase':
         return (
-          <a className="social keybase" href={`https://keybase.io/${identity.displayName}`} target="_blank" rel="noopener noreferrer">
-            <SVG src="icons/icon-keybase.svg" className="icon" />
-            {identity.displayName}
-          </a>
+          <div className="social-item">
+            <a className="social keybase" href={`https://keybase.io/${identity.displayName}`} target="_blank" rel="noopener noreferrer">
+              <SVG src="icons/icon-keybase.svg" className="icon" />
+              {identity.displayName}
+            </a>
+          </div>
+          
         )
       default:
         return null
