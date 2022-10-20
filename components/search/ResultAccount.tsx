72dab9dd71c66fcc6209eab7241ecc53522d62d3
billy <Billy@mask.io>
2022-10-20 16:18:09 +0800

    fix: src

--- components/search/ResultAccount.tsx
+++ components/search/ResultAccount.tsx
@@ -1,6 +1,6 @@
 import React, { memo, useState } from "react";
 import { ResultAccountItem } from "./ResultAccountItem";
-import { ResultGraph } from "./ResultGraph";
+import { ResultGraph } from "../graph/ResultGraph";
 import { formatText } from "../../utils/utils";
 import SVG from "react-inlinesvg";
 
