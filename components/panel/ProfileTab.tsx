7ca20eef33d90637b3cf57a6778bd4d8e7bddd56
billy <Billy@mask.io>
2022-10-22 13:36:29 +0800

    fix: some

--- components/panel/ProfileTab.tsx
+++ components/panel/ProfileTab.tsx
@@ -1,9 +1,17 @@
-import { memo } from "react"
+import { memo } from "react";
 
-const RenderProfileTab = ()=>{
-    return <div>
-        Profile
+const RenderProfileTab = () => {
+  return (
+    <div>
+      <div className="profile-description">
+        sujiyan.eth founder of @realmasknetwork $Mask; maintain mstdn.jp
+        mastodon.cloud ; Engineer; Journalist; FOSS/Anti996; 中文/日本語
+      </div>
+      <div className="records">
+        <div className="record-item">222</div>
+      </div>
     </div>
-}
+  );
+};
 
-export const ProfileTab = memo(RenderProfileTab)
+export const ProfileTab = memo(RenderProfileTab);
