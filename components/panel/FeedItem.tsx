a4d313e8775dea575649653428fee4d037c0d35b
billy <Billy@mask.io>
2022-11-09 15:54:25 +0800

    feat: comment card

--- components/panel/FeedItem.tsx
+++ components/panel/FeedItem.tsx
@@ -1,8 +1,11 @@
 import { memo } from "react";
-import { CollectibleCard, isCollectibleFeed } from "./feedCards/CollectibleCard";
+import {
+  CollectibleCard,
+  isCollectibleFeed,
+} from "./feedCards/CollectibleCard";
 import { CommentCard, isCommentFeed } from "./feedCards/CommentCard";
 import { DonationCard, isDonationFeed } from "./feedCards/DonationCard";
-import { isLiquidityFeed,LiquidityCard  } from "./feedCards/LiqudityCard";
+import { isLiquidityFeed, LiquidityCard } from "./feedCards/LiqudityCard";
 import { isNoteFeed, NoteCard } from "./feedCards/NoteCard";
 import { isProfileFeed, ProfileCard } from "./feedCards/ProfileCard";
 import { isProposeFeed, ProposeCard } from "./feedCards/ProposeCard";
@@ -17,23 +20,25 @@ const RenderFeedItem = (props) => {
   const { feed, identity } = props;
   if (isTokenOperationFeed(feed))
     return <TokenOperationCard feed={feed} identity />;
-  if (isTokenSwapFeed(feed)) return <TokenSwapCard feed={feed} identity />;
+  if (isTokenSwapFeed(feed))
+    return <TokenSwapCard feed={feed} identity={identity} />;
 
-  if (isLiquidityFeed(feed)) return <LiquidityCard feed={feed}  />;
+  if (isLiquidityFeed(feed)) return <LiquidityCard feed={feed} />;
 
-  if (isCollectibleFeed(feed)) return <CollectibleCard feed={feed}  />
+  if (isCollectibleFeed(feed)) return <CollectibleCard feed={feed} />;
 
-  if (isDonationFeed(feed)) return <DonationCard feed={feed}  />
+  if (isDonationFeed(feed)) return <DonationCard feed={feed} />;
 
-  if (isNoteFeed(feed)) return <NoteCard feed={feed} identity  />
+  if (isNoteFeed(feed)) return <NoteCard feed={feed} identity={identity} />;
 
-  if (isCommentFeed(feed)) return <CommentCard feed={feed}  />
+  if (isCommentFeed(feed))
+    return <CommentCard feed={feed} identity={identity} />;
 
-  if (isProfileFeed(feed)) return <ProfileCard feed={feed}  />
+  if (isProfileFeed(feed)) return <ProfileCard feed={feed} />;
 
-  if (isProposeFeed(feed)) return <ProposeCard feed={feed}  />
+  if (isProposeFeed(feed)) return <ProposeCard feed={feed} />;
 
-  if (isVoteFeed(feed)) return <VoteCard feed={feed}  />
+  if (isVoteFeed(feed)) return <VoteCard feed={feed} />;
 };
 
 export const FeedItem = memo(RenderFeedItem);
--- components/panel/feedCards/CommentCard.tsx
+++ components/panel/feedCards/CommentCard.tsx
@@ -1,14 +1,48 @@
 import { memo } from "react";
+import { formatText } from "../../../utils/utils";
 import { Tag, Type } from "../../apis/rss3/types";
 
 export function isCommentFeed(feed) {
-    return feed.tag === Tag.Social && feed.type === Type.Comment
+  return feed.tag === Tag.Social && feed.type === Type.Comment;
 }
 
-const RenderCommentFeed = (props)=>{
-    return <div>
-        comment
+const RenderCommentFeed = (props) => {
+  const { feed, identity } = props;
+
+  const action = feed.actions[0];
+  const metadata = action.metadata;
+
+  const user = identity.identity;
+  const commentTarget = metadata?.targetuseAddressLabel;
+  return (
+    <div className="feed-item-box">
+      <div className="feed-type-badge"></div>
+      <div className="feed-item">
+        <div className="feed-item-header">
+          <div className="feed-type-intro">
+            <div className="strong">{formatText(user ?? "")}</div>
+            made a comment on
+            <div className="strong">{action.platform || "unknown"}</div>
+          </div>
+        </div>
+        <div>{metadata?.body}</div>
+
+        {commentTarget && (
+          <div className={"feed-item-main"}>
+            <picture>
+              <img
+                className="feed-nft-img"
+                style={{ width: 64 }}
+                src={commentTarget.media[0].address}
+                alt="comment"
+              />
+            </picture>
+            <div className="feed-nft-info">{commentTarget?.body}</div>
+          </div>
+        )}
+      </div>
     </div>
-}
+  );
+};
 
-export const CommentCard = memo(RenderCommentFeed)
+export const CommentCard = memo(RenderCommentFeed);
