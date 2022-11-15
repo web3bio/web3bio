ae8704f8ca9bb4e715254c95244cec52f1b9911f
billy <Billy@mask.io>
2022-11-15 19:36:12 +0800

    fix: some style fix

--- components/panel/IdentityPanel.tsx
+++ components/panel/IdentityPanel.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
-import { memo, useEffect, useState } from "react";
+import { memo, useState } from "react";
 import SVG from "react-inlinesvg";
-import { useAsync } from "react-use";
+import Clipboard from "react-clipboard.js";
 import { getEnumAsArray } from "../../utils/utils";
 import { FeedsTab } from "./FeedsTab";
 import { NFTsTab } from "./NFTsTab";
@@ -17,14 +16,16 @@ const IdentityPanelRender = (props) => {
   const { onClose, identity, onTabChange, curTab } = props;
   const [activeTab, setActiveTab] = useState(curTab || TabsMap.profile);
   const [curAsset, setCurAsset] = useState(null);
+  const [copied, setCopied] = useState(null);
+  const onCopySuccess = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
   const renderContent = () => {
     return {
-      [TabsMap.profile]: (
-        <ProfileTab
-          identity={identity}
-         
-        />
-      ),
+      [TabsMap.profile]: <ProfileTab identity={identity} />,
       [TabsMap.feeds]: <FeedsTab identity={identity} />,
       [TabsMap.nfts]: (
         <NFTsTab
@@ -57,12 +58,15 @@ const IdentityPanelRender = (props) => {
           <div className="displayName">{identity.displayName}</div>
           <div className="identity">
             {identity.identity}
-            <SVG
-              className="copy-icon"
-              src="icons/icon-copy.svg"
-              width={16}
-              height={16}
-            />
+            <Clipboard
+              component="div"
+              className="action"
+              data-clipboard-text={identity.identity}
+              onSuccess={onCopySuccess}
+            >
+              <SVG src="icons/icon-copy.svg" width={20} height={20} />
+              {copied && <div className="tooltip-copy">COPIED</div>}
+            </Clipboard>
           </div>
         </div>
       </div>
