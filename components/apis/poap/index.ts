3e405c0da1c204337d1323e780e9cbb08c48a44b
billy <Billy@mask.io>
2022-11-18 13:51:16 +0800

    feat: poap api key injected

--- components/apis/poap/index.ts
+++ components/apis/poap/index.ts
@@ -1,4 +1,4 @@
-const AUTHENTICATION = undefined;
+const AUTHENTICATION = process.env.POAP_APAI_KEY;
 export const POAP_END_POINT = "https://api.poap.tech/actions/scan/";
 export const POAPFetcher = async (url) => {
   const res = await fetch(url, {
