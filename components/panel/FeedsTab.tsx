07525b8c945d683b96496f233b33428400044256
billy <Billy@mask.io>
2022-11-29 18:54:08 +0800

    fix: useSWRInfinite loop

--- components/panel/FeedsTab.tsx
+++ components/panel/FeedsTab.tsx
@@ -1,75 +1,100 @@
 import { memo, useEffect, useRef, useState } from "react";
-import useSWR from "swr";
 import { RSS3Fetcher, RSS3_END_POINT } from "../apis/rss3";
 import { Empty } from "../shared/Empty";
 import { Loading } from "../shared/Loading";
 import { Error } from "../shared/Error";
 import { FeedItem, isSupportedFeed } from "./FeedItem";
 import { formatTimestamp } from "../../utils/date";
+import useSWRInfinite from "swr/infinite";
+
+const PAGE_SIZE = 30;
+const getFeedsURL = (
+  address: string,
+  startHash?: string,
+  previousPageData?
+) => {
+  if (previousPageData && !previousPageData.length) return null;
+  return (
+    RSS3_END_POINT +
+    `notes/${address}?limit=${PAGE_SIZE}${
+      startHash ? `&cursor=${startHash}` : ""
+    }&&include_poap=false&count_only=false&query_status=false`
+  );
+};
 
 function useFeeds(address: string, startHash?: string) {
-  const { data, error } = useSWR<any>(
-    RSS3_END_POINT +
-      `notes/${address}?limit=30${
-        startHash ? `&cursor=${startHash}` : ""
-      }&&include_poap=false&count_only=false&query_status=false`,
+  const { data, error, size, isValidating, setSize } = useSWRInfinite(
+    () => getFeedsURL(address, startHash),
     RSS3Fetcher
   );
   return {
-    data: data,
+    data: (data && data[0].result) || [],
     isLoading: !error && !data,
-    isError: error,
+    error,
+    size,
+    setSize,
+    isValidating,
   };
 }
+
 const RenderFeedsTab = (props) => {
   const { identity } = props;
   const [startHash, setStartHash] = useState("");
-  const [showData, setShowData] = useState([]);
-  const { data, isLoading, isError } = useFeeds(identity.identity, startHash);
+  const { data, error, size, setSize } = useFeeds(identity.identity, startHash);
+  const issues = data ? [].concat(...data) : [];
+  const isLoadingInitialData = !data && !error;
+  const isLoadingMore =
+    isLoadingInitialData ||
+    (size > 0 && data && typeof data[size - 1] === "undefined");
+  const isEmpty = data?.[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty || (data && data[data.length - 1]?.length < PAGE_SIZE);
+
   const ref = useRef(null);
   useEffect(() => {
-    if (data && data.result) {
-      const arr = showData.concat(data.result);
-      setShowData(Array.from(new Set(arr)));
-    }
     const scrollLoad = (e) => {
-      //可视区高度
-      let scrollHeight = e.target.scrollHeight;
-      //滚动高度
-      let scrollTop = e.target.scrollTop;
-      //列表内容实际高度
-      let offsetHeight = e.target.offsetHeight;
+      const scrollHeight = e.target.scrollHeight;
+      const scrollTop = e.target.scrollTop;
+      const offsetHeight = e.target.offsetHeight;
       if (offsetHeight + scrollTop >= scrollHeight) {
-        if (showData.length > 0) {
-          const len = showData.length;
-          setStartHash(showData[len - 1].hash);
+        if (issues.length > 0) {
+          if (isReachingEnd) return;
+          const len = issues.length;
+          console.log(issues, "issues");
+          setStartHash(issues[len - 1].hash);
+          setSize(size + 1);
         }
       }
     };
     if (ref.current) {
       ref.current.addEventListener("scroll", scrollLoad);
     }
-  }, [showData]);
-  if (isLoading) return <Loading />;
-  if (isError) return <Error text={isError} />;
-  if (!showData.length) return <Empty />;
-  console.log(data, "feeds data");
+  }, [data, isReachingEnd, issues, size, setSize,startHash]);
+
+  console.log("feeds data");
   return (
     <div className="feeds-container-box">
       <div className="feeds-title">Social Feeds</div>
+
       <div ref={ref} className="feeds-container">
-        {data.result.map((x, idx) => {
-          return (
-            isSupportedFeed(x) && (
+        {isEmpty ? <p>Yay, no issues found.</p> : null}
+        {issues.map(
+          (x, idx) =>
+            (isSupportedFeed(x) && (
               <div key={idx}>
                 <FeedItem identity={identity} feed={x} />
                 <div className="feed-timestamp">
                   {formatTimestamp(x.timestamp)}
                 </div>
               </div>
-            )
-          );
-        })}
+            )) ||
+            null
+        )}
+        {isLoadingMore
+          ? "loading..."
+          : isReachingEnd
+          ? "no more issues"
+          : "scroll to load"}
       </div>
     </div>
   );
