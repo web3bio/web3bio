c3a3bb923bddff1d688715366da1fa2e552bf954
billy <Billy@mask.io>
2023-03-31 17:02:16 +0800

    fix: init name query lens

--- pages/api/profile/lens/[handle].ts
+++ pages/api/profile/lens/[handle].ts
@@ -12,6 +12,8 @@ import client from "../../../../utils/apollo";
 import _ from "lodash";
 import { GET_PROFILE_LENS } from "../../../../utils/lens";
 import { HandleResponseData } from "../ens/types";
+import { SocialPlatformMapping } from "../../../../utils/platform";
+import { getSocialMediaLink } from "../ens/utils";
 
 export const getLensProfile = async (handle: string) => {
   const fetchRes = await client.query({
@@ -68,47 +70,70 @@ const resolveHandle = (handle: string) => {
   return handle;
 };
 
-const resolveHandleFromURL = async (
+const resolveNameFromLens = async (
   handle: string,
   res: NextApiResponse<HandleResponseData>
 ) => {
   try {
-    let address = "";
-    let name = "";
-    let avatar = null;
-    if (isAddress(handle)) {
-      address = getAddress(handle);
-      name = (await provider.lookupAddress(address)) || null;
-      avatar = (await provider.getAvatar(name)) || null;
-    } else {
-      [address, avatar] = await Promise.all([
-        provider.resolveName(handle),
-        provider.getAvatar(handle),
-      ]);
-      name = handle;
-    }
-    const res = await getLensProfile(handle);
-
+    const response = await getLensProfile(handle);
+    console.log(response, "res");
     let LINKRES = {};
     let CRYPTORES = {
-      eth: address,
-      btc: null,
-      ltc: null,
-      doge: null,
+      eth: response.ownedBy,
     };
 
+    if (response.attributes) {
+      const linksRecords = response.attributes;
+      const linksToFetch = linksRecords.reduce((pre, cur) => {
+        if (Object.keys(SocialPlatformMapping).includes(cur.key))
+          pre.push(cur.key);
+        return pre;
+      }, []);
+
+      const getLink = async () => {
+        const _linkRes = {};
+        for (let i = 0; i < linksToFetch.length; i++) {
+          const recordText = linksToFetch[i];
+          const handle = resolveHandle(
+            _.find(linksRecords, (o) => o.key === recordText).value
+          );
+          if (handle) {
+            const resolvedHandle =
+              recordText === SocialPlatformMapping.twitter.key
+                ? handle.replaceAll("@", "")
+                : handle;
+            _linkRes[recordText] = {
+              link: getSocialMediaLink(resolvedHandle, recordText),
+              handle: resolvedHandle,
+            };
+          }
+        }
+        return _linkRes;
+      };
+      LINKRES = await getLink();
+    }
+
     const resJSON = {
-      owner: address,
-      identity: name,
-      displayName: null,
-      avatar: null,
+      owner: response.ownedBy,
+      identity: response.name,
+      displayName: response.name,
+      avatar: await resolveEipAssetURL(response.picture.original.url),
       email: null,
-      description: null,
-      location: null,
+      description: response.bio,
+      location: response.attributes
+        ? _.find(response.attributes, (o) => o.key === "location").value
+        : null,
       header: null,
       notice: null,
       keywords: null,
-      url: null,
+      url: response.attributes
+        ? getSocialMediaLink(
+            resolveHandle(
+              _.find(response.attributes, (o) => o.key === "website").value
+            ),
+            SocialPlatformMapping.website.key
+          )
+        : null,
       links: LINKRES,
       addresses: CRYPTORES,
     };
@@ -149,5 +174,5 @@ export default async function handler(
     return res.redirect(307, resolve(req.url!, lowercaseAddress));
   }
 
-  return resolveHandleFromURL(lowercaseAddress, res);
+  return resolveNameFromLens(lowercaseAddress, res);
 }
