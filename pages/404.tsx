8f9ec5dab77e414db0b774f1508a3d8c8449c201
billy <Billy@mask.io>
2023-05-16 17:52:29 +0800

    chore: nft dialog improve && random emoji

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,22 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Custom404() {
-  const router = useRouter()
-
+  const [emoji, setEmoji] = useState("🤔");
+  const router = useRouter();
+  useEffect(() => {
+    setEmoji(
+      ["🤔", "😱", "😵‍💫", "😵", "🤦‍♀️", "💆‍♂️", "🤷‍♂️", "🙇‍♂️"][
+        Math.floor(Math.random() * 8)
+      ]
+    );
+  }, []);
   return (
     <main className="web3-profile container grid-xl">
       <div className="empty error-404">
-        <div className="empty-icon h1" style={{"fontSize": "72px"}}>🤔</div>
+        <div className="empty-icon h1" style={{ fontSize: "72px" }}>
+          {emoji}
+        </div>
         <p className="empty-title h4">Not found</p>
         <p className="empty-subtitle">This page could not be found.</p>
         <button className="btn btn-primary mt-4" onClick={() => router.back()}>
