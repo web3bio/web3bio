16406e85d58377733485fbadbe757821c97cb216
billy <Billy@mask.io>
2023-06-20 15:26:16 +0800

    chore: fufill

--- components/profile/MirrorWidget.tsx
+++ components/profile/MirrorWidget.tsx
@@ -1,7 +1,9 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import useSWR from "swr";
 import { PlatformType } from "../../utils/platform";
 import { RSS3Fetcher, RSS3_END_POINT } from "../apis/rss3";
+import Markdown from "react-markdown";
+
 const useArticles = (address: string) => {
   const { data, error } = useSWR<any>(
     `${RSS3_END_POINT}notes/${address}?limit=500&platform=${PlatformType.mirror}&include_poap=false&count_only=false&query_status=false`,
@@ -19,24 +21,43 @@ const RenderMirrorWidget = (props) => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
     if (data?.result) {
-      setArticles(Array.from(data.result));
+      console.log(data, "kkk");
+      setArticles(
+        data.result?.map((x) => {
+          return {
+            ...x.actions?.[0],
+            date: x.timestamp,
+          };
+        })
+      );
     }
   }, [data]);
   if (!articles || isLoading) return null;
   return (
-    <div className="profile-widget-full profile-widget profile-widget-nft">
-      <h2 className="profile-widget-title">
-        <span className="emoji-large mr-2">📖</span>
-        Articles
-      </h2>
-      {articles.map((x, idx) => {
-        console.log("article", x.actions[0]);
-        return (
-          <div key={idx}>
-            {idx + 1}: {x.actions?.[0].metadata.title}
-          </div>
-        );
-      })}
+    <div className="profile-widget-full ">
+      <div className="profile-widget profile-widget-nft" style={{
+        maxHeight:'20rem',
+        overflowY:'auto'
+      }} >
+        <h2 className="profile-widget-title">
+          <span className="emoji-large mr-2">📖</span>
+          Articles
+        </h2>
+        {articles?.map((x, idx) => {
+          return (
+            <div key={`${idx}${x?.metadata.title}`}>
+              <div className="article-title">
+                <p>
+                  {idx + 1}: {x?.metadata.title}
+                </p>
+                <p>{x.date}</p>
+              </div>
+              {/*Content Here*/}
+              <Markdown className="markdown" >{x?.metadata.body}</Markdown>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
