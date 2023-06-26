431d9a711d43be8999a9119f7131506c1a92f0d9
billy <Billy@mask.io>
2023-06-26 16:56:17 +0800

    feat: ssr initial loading state

---
+++ app/[domain]/loading.tsx
@@ -0,0 +1,8 @@
+import { Loading } from "../../components/shared/Loading";
+export default function LoadingPage() {
+  return (
+    <div className="web3-profile container grid-xl global-loading">
+      <Loading />
+    </div>
+  );
+}
