62834117c1d5f5d5126507cc7a3da4ad5a927d8f
billy <Billy@mask.io>
2023-06-27 14:48:28 +0800

    reafactor: refactor next-seo to nextjs 13 built in seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,63 @@
-"use client";
-import { ApolloProvider } from "@apollo/client";
-import { GoogleAnalytics } from "nextjs-google-analytics";
-import { DefaultSeo } from "next-seo";
-import { usePathname } from "next/navigation";
 import Head from "next/head";
-import client from "../utils/apollo";
+import { Footer } from "../components/shared/Footer";
 import "../styles/web3bio.scss";
+import { headers } from "next/headers";
 
+export async function generateMetadata() {
+  const headerList = headers();
+  const domain = headerList.get("host") || "";
+  const fullUrl = headerList.get("referer") || "";
+  const [, pathname] =
+    fullUrl.match(new RegExp(`https?:\/\/${domain}(.*)`)) || [];
+  return {
+    title:
+      "Web3.bio - Web3 Identity Graph Search and Link-in-bio Profile Service",
+    description:
+      "Web3.bio (Previously Web5.bio) is a Web3 and Web 2.0 Identity Graph search and link in bio profile platform. Web3.bio will provide a list of relevant identities when you are searching any Twitter handle, Ethereum address, ENS domain, Lens profile or Unstoppable Domains, and other Web3 identities.",
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`,
+    },
+    openGraph: {
+      type: "website",
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`,
+      siteName: "Web3.bio",
+      title:
+        "Web3.bio - Web3 Identity Graph Search and Link-in-bio Profile Service",
+      description:
+        "Web3.bio (Previously Web5.bio) is a Web3 and Web 2.0 Identity Graph search and link in bio profile platform. Web3.bio will provide a list of relevant identities when you are searching any Twitter handle, Ethereum address, ENS domain, Lens profile or Unstoppable Domains, and other Web3 identities.",
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_BASE_URL}/img/web3bio-social.jpg`,
+        },
+      ],
+    },
+  };
+}
 export default function RootLayout({ children, modal }) {
-  const asPath = usePathname();
-  let pageLink = `${process.env.NEXT_PUBLIC_BASE_URL}${asPath}`;
   return (
     <html>
-      <ApolloProvider client={client}>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="robots" content="index, follow" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
-          />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="google-site-verification"
-            content="iaUpA0X2l6UNb8C38RvUe4i_DOMvo5Ciqvf6MtYjzPs"
-          />
-          <link rel="shortcut icon" href="/favicon.ico" />
-        </Head>
-        <DefaultSeo
-          title="Web3.bio - Web3 Identity Graph Search and Link-in-bio Profile Service"
-          description="Web3.bio (Previously Web5.bio) is a Web3 and Web 2.0 Identity Graph search and link in bio profile platform. Web3.bio will provide a list of relevant identities when you are searching any Twitter handle, Ethereum address, ENS domain, Lens profile or Unstoppable Domains, and other Web3 identities."
-          canonical={pageLink}
-          openGraph={{
-            type: "website",
-            url: pageLink,
-            site_name: "Web3.bio",
-            title:
-              "Web3.bio - Web3 Identity Graph Search and Link-in-bio Profile Service",
-            description:
-              "Web3.bio (Previously Web5.bio) is a Web3 and Web 2.0 Identity Graph search and link in bio profile platform. Web3.bio will provide a list of relevant identities when you are searching any Twitter handle, Ethereum address, ENS domain, Lens profile or Unstoppable Domains, and other Web3 identities.",
-            images: [
-              {
-                url: `${process.env.NEXT_PUBLIC_BASE_URL}/img/web3bio-social.jpg`,
-              },
-            ],
-          }}
-          twitter={{
-            handle: "@web3bio",
-            site: "@web3bio",
-            cardType: "summary",
-          }}
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
         />
-        <body>
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="google-site-verification"
+          content="iaUpA0X2l6UNb8C38RvUe4i_DOMvo5Ciqvf6MtYjzPs"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <div className="web3bio-container">
+          <div className="web3bio-cover flare"></div>
           {children}
           {modal}
-        </body>
-        <GoogleAnalytics strategy="lazyOnload" />
-      </ApolloProvider>
+        </div>
+        <Footer />
+      </body>
     </html>
   );
 }
