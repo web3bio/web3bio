b5cf12270cbbf8e2cb611d2d8f5a0a5376685653
billy <Billy@mask.io>
2023-06-30 17:16:57 +0800

    fix: some

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,35 @@
-import Head from "next/head";
 import "../styles/web3bio.scss";
 import { headers } from "next/headers";
+import { Metadata } from "next";
 
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
   const headerList = headers();
   const domain = headerList.get("host") || "";
   const fullUrl = headerList.get("referer") || "";
   const [, pathname] =
     fullUrl.match(new RegExp(`https?:\/\/${domain}(.*)`)) || [];
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "https://web3.bio/";
   return {
-    metadataBase: new URL(
-      process.env.NEXT_PUBLIC_BASE_URL || "https://web3.bio"
-    ),
+    metadataBase: new URL(baseURL),
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      viewportFit: "auto",
+    },
+    robots: "index, follow",
+    verification: {
+      google: "iaUpA0X2l6UNb8C38RvUe4i_DOMvo5Ciqvf6MtYjzPs",
+    },
     title:
       "Web3.bio - Web3 Identity Graph Search and Link-in-bio Profile Service",
     description:
       "Web3.bio (Previously Web5.bio) is a Web3 and Web 2.0 Identity Graph search and link in bio profile platform. Web3.bio will provide a list of relevant identities when you are searching any Twitter handle, Ethereum address, ENS domain, Lens profile or Unstoppable Domains, and other Web3 identities.",
     alternates: {
-      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`,
+      canonical: `${baseURL}${pathname || ""}`,
     },
     openGraph: {
       type: "website",
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`,
+      url: `${baseURL}${pathname || ""}`,
       siteName: "Web3.bio",
       title:
         "Web3.bio - Web3 Identity Graph Search and Link-in-bio Profile Service",
@@ -29,7 +37,7 @@ export async function generateMetadata() {
         "Web3.bio (Previously Web5.bio) is a Web3 and Web 2.0 Identity Graph search and link in bio profile platform. Web3.bio will provide a list of relevant identities when you are searching any Twitter handle, Ethereum address, ENS domain, Lens profile or Unstoppable Domains, and other Web3 identities.",
       images: [
         {
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}/img/web3bio-social.jpg`,
+          url: `${baseURL}/img/web3bio-social.jpg`,
         },
       ],
     },
@@ -38,20 +46,6 @@ export async function generateMetadata() {
 export default function RootLayout({ children, modal }) {
   return (
     <html>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="robots" content="index, follow" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
-        />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="google-site-verification"
-          content="iaUpA0X2l6UNb8C38RvUe4i_DOMvo5Ciqvf6MtYjzPs"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
       <body>
         <main>
           {children}
