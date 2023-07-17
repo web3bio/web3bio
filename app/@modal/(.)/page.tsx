2941417cfc8479de833cea7704acf3f0e8a2da21
billy <Billy@mask.io>
2023-07-17 15:40:13 +0800

    fix: staging feedback

--- app/@modal/(.)/page.tsx
+++ app/@modal/(.)/page.tsx
@@ -10,15 +10,21 @@ import { useEffect, useState } from "react";
 import { PlatformType } from "../../../utils/platform";
 import Modal from "../../../components/shared/Modal";
 
-function useProfile(identity: string, platform: string, fallbackData) {
+function useProfile(
+  shouldFetch: boolean,
+  identity: string,
+  platform: string,
+  fallbackData
+) {
   const resolvedIdentity =
     platform === PlatformType.farcaster
       ? identity.replaceAll(".farcaster", "")
       : identity;
-  const url =
-    process.env.NEXT_PUBLIC_PROFILE_END_POINT +
-    `/profile/${platform.toLowerCase()}/${resolvedIdentity}`;
-  const { data, error, isValidating } = useSWR<any>(url, _fetcher, {
+  const url = shouldFetch
+    ? process.env.NEXT_PUBLIC_PROFILE_END_POINT +
+      `/profile/${platform.toLowerCase()}/${resolvedIdentity}`
+    : null;
+  const { data, error, isValidating } = useSWR(url, _fetcher, {
     fallbackData: fallbackData,
   });
   return {
@@ -32,8 +38,13 @@ export default function ProfileModal() {
   const pathName = usePathname();
   const domain = pathName.replace("/", "");
   const platform = handleSearchPlatform(domain);
-  const { data, isLoading, isError } = useProfile(domain, platform, null);
   const [showModal, setShowModal] = useState(false);
+  const { data, isLoading, isError } = useProfile(
+    showModal,
+    domain,
+    platform,
+    null
+  );
   const router = useRouter();
   useEffect(() => {
     if (pathName.length > 1) setShowModal(true);
@@ -57,7 +68,7 @@ export default function ProfileModal() {
               links: Object.entries(data?.links || {}).map(([key, value]) => {
                 return {
                   platform: key,
-                  ...(value as any),
+                  ...(value as { handle: string; link: string }),
                 };
               }),
             }}
