327fd9ccefcaae23deda08ca3eb820ee8668e066
billy <Billy@mask.io>
2023-07-28 17:26:37 +0800

    chore: refact modal view

--- app/@modal/(.)/page.tsx
+++ app/@modal/(.)/page.tsx
@@ -1,76 +1,109 @@
 "use client";
 import useSWR from "swr";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { _fetcher } from "../../../components/apis/ens";
+import { handleSearchPlatform, mapLinks } from "../../../utils/utils";
+import { usePathname } from "next/navigation";
+import { PlatformType } from "../../../utils/platform";
 import { Loading } from "../../../components/shared/Loading";
 import { Error } from "../../../components/shared/Error";
 import ProfileMain from "../../../components/profile/ProfileMain";
-import { _fetcher } from "../../../components/apis/ens";
-import { handleSearchPlatform, mapLinks } from "../../../utils/utils";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { PlatformType } from "../../../utils/platform";
 import Modal from "../../../components/shared/Modal";
 
-function useProfile(shouldFetch: boolean, identity: string, fallbackData) {
+interface ProfileData {
+  platform: PlatformType;
+  identity: string;
+  name: string;
+  bio: string;
+}
+interface UseProfileProps {
+  shouldFetch?: boolean;
+  identity: string;
+  fallbackData?: ProfileData[];
+}
+
+function useProfile({
+  shouldFetch,
+  identity,
+  fallbackData,
+}: UseProfileProps) {
   const url = shouldFetch
-    ? process.env.NEXT_PUBLIC_PROFILE_END_POINT + `/profile/${identity}`
+    ? `${process.env.NEXT_PUBLIC_PROFILE_END_POINT}/profile/${identity}`
     : null;
-  const { data, error, isValidating } = useSWR(url, _fetcher, {
-    fallbackData: fallbackData,
-  });
+
+  const { data, error, isValidating } = useSWR(url, _fetcher);
   return {
-    data: data,
+    data,
     isLoading: isValidating || (!data && !error),
     isError: error,
   };
+}
+
+function findProfileData(
+  domain: string,
+  data: ProfileData[] | undefined,
+  platform: PlatformType
+) {
+  if (!domain || !data) return null;
+  const profiles = data.filter((x) => x.platform === platform);
+  return profiles.length > 0 ? profiles[0] : data[0];
 }
 
 export default function ProfileModal() {
   const pathName = usePathname();
   const domain = pathName.replace("/", "");
   const platform = handleSearchPlatform(domain);
-  const [profileData, setProfileData] = useState(null);
-  const { data, isLoading, isError } = useProfile(
-    !!domain &&
-      [PlatformType.ens, PlatformType.lens, PlatformType.farcaster,PlatformType.nextid].includes(
-        platform
-      ),
-    domain,
-    null
-  );
+  const [profileData, setProfileData] = useState<ProfileData | null>(null);
+  const { data, isLoading, isError } = useProfile({
+    shouldFetch:
+      !!domain &&
+      [
+        PlatformType.ens,
+        PlatformType.lens,
+        PlatformType.farcaster,
+        PlatformType.nextid,
+      ].includes(platform),
+    identity: domain,
+  });
   const router = useRouter();
+
   useEffect(() => {
-    if (domain && data?.length > 0) {
-      setProfileData(data?.find((x) => x.platform === platform) || data?.[0]);
-    } else {
-      setProfileData(null);
-    }
+    setProfileData(findProfileData(domain, data, platform));
   }, [domain, data, platform]);
-  return (
-    (profileData && (
-      <Modal onDismiss={() => router.back()}>
-        {isLoading ? (
-          <div className="global-loading">
-            <Loading />
-          </div>
-        ) : isError ? (
-          <Error />
-        ) : (
-          <ProfileMain
-            data={{
-              ...(data?.find((x) => x.platform === platform) || data?.[0]),
-              links: mapLinks(data),
-            }}
-            relations={Array.from(
-              data?.map((x) => ({
-                platform: x.platform,
-                identity: x.identity,
-              }))
-            )}
-            platform={platform}
-          />
-        )}
-      </Modal>
-    )) ||
-    null
-  );
+
+  const renderModalContent = () => {
+    if (isLoading) {
+      return (
+        <div className="global-loading">
+          <Loading />
+        </div>
+      );
+    }
+
+    if (isError) {
+      return <Error />;
+    }
+
+    if (!profileData) {
+      return null;
+    }
+
+    return (
+      <ProfileMain
+        data={{
+          ...profileData,
+          links: mapLinks(data || []),
+        }}
+        relations={
+          data?.map((x) => ({ platform: x.platform, identity: x.identity })) ||
+          []
+        }
+        platform={platform}
+      />
+    );
+  };
+  return profileData ? (
+    <Modal onDismiss={() => router.back()}>{renderModalContent()}</Modal>
+  ) : null;
 }
