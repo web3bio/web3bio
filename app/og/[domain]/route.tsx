130f675a5d72ea98d8765aa56b800ff12ea8ae5f
billy <Billy@mask.io>
2023-08-04 13:30:04 +0800

    chore: empty avatar fallback

--- app/og/[domain]/route.tsx
+++ app/og/[domain]/route.tsx
@@ -21,113 +21,122 @@ const fetchProfile = async (domain: string) => {
 
 // Image generation
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const domain = searchParams.get("domain");
-  if (!domain) return null;
-  const data = await fetchProfile(domain);
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-          height: "100%",
-          width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
-          letterSpacing: "-.02em",
-          background: "#fff",
-          backgroundImage:
-            "radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)",
-          backgroundSize: "100px 100px",
-        }}
-      >
+  try {
+    const { searchParams } = new URL(request.url);
+    const domain = searchParams.get("domain");
+    if (!domain) return null;
+    const data = await fetchProfile(domain);
+    const avatarURI = data.avatar
+      ? await fetch(new URL(data.avatar))
+          .then((res) => res.arrayBuffer())
+          .catch(() => "https://web3.bio/logo-web3bio.png")
+      : "https://web3.bio/logo-web3bio.png";
+    return new ImageResponse(
+      (
         <div
           style={{
-            left: 42,
-            top: 42,
-            position: "absolute",
             display: "flex",
+            height: "100%",
+            width: "100%",
             alignItems: "center",
+            justifyContent: "center",
+            letterSpacing: "-.02em",
+            background: "#fff",
+            backgroundImage:
+              "radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)",
+            backgroundSize: "100px 100px",
           }}
         >
-          <img
-            width={24}
-            height={24}
-            src="https://web3.bio/logo-web3bio.png"
-            alt=""
-          />
-          <span
+          <div
             style={{
-              marginLeft: 8,
-              fontSize: 20,
+              left: 42,
+              top: 42,
+              position: "absolute",
+              display: "flex",
+              alignItems: "center",
             }}
           >
-            Web3.bio
-          </span>
-        </div>
+            <img
+              width={24}
+              height={24}
+              src="https://web3.bio/logo-web3bio.png"
+              alt=""
+            />
+            <span
+              style={{
+                marginLeft: 8,
+                fontSize: 20,
+              }}
+            >
+              Web3.bio
+            </span>
+          </div>
 
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: 80,
-            padding: "20px 50px",
-            margin: "0 42px",
-            fontSize: 48,
-            lineHeight: 1.4,
-            textAlign: "center",
-          }}
-        >
-          <img
-            width={"200px"}
-            height={"200px"}
-            src={data?.avatar || "https://web3.bio/logo-web3bio.png"}
-            style={{
-              borderRadius: "50%",
-              boxShadow: "inset 0 0 6px 6px rgba(255, 255, 255, .1)",
-            }}
-            alt={domain}
-          />
           <div
             style={{
               display: "flex",
-              flexDirection: "column",
-              gap: "18px",
+              alignItems: "center",
+              gap: 80,
+              padding: "20px 50px",
+              margin: "0 42px",
+              fontSize: 48,
+              lineHeight: 1.4,
+              textAlign: "center",
             }}
           >
-            <div>{data.displayName}</div>
-            <div>{data.identity}</div>
+            <img
+              width={"200px"}
+              height={"200px"}
+              src={avatarURI as unknown as string}
+              style={{
+                borderRadius: "50%",
+                boxShadow: "inset 0 0 6px 6px rgba(255, 255, 255, .1)",
+              }}
+              alt={domain}
+            />
             <div
               style={{
                 display: "flex",
-                alignItems: "center",
-                gap: "12px",
+                flexDirection: "column",
+                gap: "18px",
               }}
             >
-              {Object.keys(data?.links || {}).map((x) => {
-                return (
-                  <img
-                    style={{
-                      color: "#fff",
-                    }}
-                    key={x}
-                    width={36}
-                    height={36}
-                    src={`https://web3.bio/${
-                      SocialPlatformMapping(x as PlatformType).icon
-                    }`}
-                    alt="platform"
-                  />
-                );
-              })}
+              <div>{data.displayName}</div>
+              <div>{data.identity}</div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "12px",
+                }}
+              >
+                {Object.keys(data?.links || {}).map((x) => {
+                  return (
+                    <img
+                      style={{
+                        color: "#fff",
+                      }}
+                      key={x}
+                      width={36}
+                      height={36}
+                      src={`https://web3.bio/${
+                        SocialPlatformMapping(x as PlatformType).icon
+                      }`}
+                      alt="platform"
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    ),
-    {
-      ...size,
-    }
-  );
+      ),
+      {
+        ...size,
+      }
+    );
+  } catch (e) {
+    console.log(e, "error");
+    return null;
+  }
 }
