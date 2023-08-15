413a2cfe186b0ffd9d060ccb47ef649d93e4ff8a
billy <Billy@mask.io>
2023-08-15 15:25:03 +0800

    chore: some

--- utils/apollo.ts
+++ utils/apollo.ts
@@ -1,8 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER,
-    cache: new InMemoryCache(),
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_SERVER,
+  headers: {
+    "x-api-key": process.env.NEXT_PUBLIC_RELATION_API_KEY ?? "",
+  },
+  cache: new InMemoryCache(),
 });
 
-export default client
+export default client;
