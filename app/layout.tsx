be2776c1c4736a97f6e543037a844125c8e12709
Yan <picturepan2@hotmail.com>
2023-09-07 17:37:27 +0800

    Minor update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import GoogleAnalytic from "../components/shared/GoogleAnalytic";
+import GoogleAnalytics from "../components/shared/GoogleAnalytics";
 import "../styles/web3bio.scss";
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -64,7 +64,7 @@ export default function RootLayout({ children, modal }) {
         <main>
           {children}
           {modal}
-          <GoogleAnalytic />
+          <GoogleAnalytics />
         </main>
       </body>
     </html>
--- components/shared/GoogleAnalytic.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-import { GoogleAnalytics } from "nextjs-google-analytics";
-export default function GoogleAnalytic() {
-  return <GoogleAnalytics strategy="lazyOnload" />;
-}
