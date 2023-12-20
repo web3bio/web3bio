80ea5fe7ea3d742d80c3678b325c95607527dc6b
billy <Billy@mask.io>
2023-12-20 12:56:53 +0800

    chore: code style

--- components/shared/Avatar.tsx
+++ components/shared/Avatar.tsx
@@ -1,28 +1,21 @@
 import { AssetPlayerProps, NFTAssetPlayer } from "./NFTAssetPlayer";
-import BoringAvatar from "boring-avatars";
 interface Avatar extends AssetPlayerProps {
-  identity?: string
+  identity?: string;
 }
 
 export const Avatar = (props: Avatar) => {
   const { src, width, height, alt, identity } = props;
-
-  return src ? (
-      <NFTAssetPlayer
-        type="image/png"
-        className="avatar"
-        src={src}
-        width={width}
-        height={height}
-        alt={alt}
-        placeholder={true}
-      />
-    ) : (
-      <BoringAvatar
-        size="100%"
-        name={identity}
-        variant="bauhaus"
-        colors={["#ECD7C8", "#EEA4BC", "#BE88C4", "#9186E7", "#92C9F9"]}
-      />
-    );
+  const profileAvatarAPIURL =
+    process.env.NEXT_PUBLIC_PROFILE_END_POINT + `/avatar/${identity}`;
+  return (
+    <NFTAssetPlayer
+      type="image/png"
+      className="avatar"
+      src={src || profileAvatarAPIURL}
+      width={width}
+      height={height}
+      alt={alt}
+      placeholder={true}
+    />
+  );
 };
