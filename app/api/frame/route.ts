993c90730f6c19e0db7266feb16f5ec0c2fc9068
billy <Billy@mask.io>
2024-02-06 23:47:22 +0800

    chore: route

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect(
-    process.env.NEXT_PUBLIC_BASE_URL || "https://web3.bio",
-    { status: 302 }
-  );
+  const headers = new Headers();
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://web3.bio";
+  headers.set("Location", baseUrl);
+  return NextResponse.redirect(baseUrl, { headers, status: 302 });
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
