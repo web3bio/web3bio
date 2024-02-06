c7bf29f6330528903b5daa877f4d3f6eb74d4622
billy <Billy@mask.io>
2024-02-06 22:40:50 +0800

    chore: some

--- app/[domain]/page.tsx
+++ app/[domain]/page.tsx
@@ -100,6 +100,7 @@ export async function generateMetadata({
       fcMetadata[`fc:frame:button:${index + 1}`] = x.identity;
       fcMetadata[`fc:frame:button:${index + 1}:content`] = "post_redirect";
     });
+  
   if (data.length === 0 || data.length > 3) {
     fcMetadata["fc:frame:post_url"] = `${baseURL}/${domain}`;
     fcMetadata[`fc:frame:button:${(data.length = 0 ? 1 : 4)}`] = "More";
