839bddfb76e035f38868535c4e5b87177e082e7e
billy <Billy@mask.io>
2024-02-07 13:10:57 +0800

    chore: cors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  headers: async () => [
+    {
+      source: "/api/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
