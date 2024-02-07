4a6c4336034f8bbf87de6cee287dacc1900292be
billy <Billy@mask.io>
2024-02-07 16:49:03 +0800

    chore: clean

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  headers: async () => [
-    {
-      source: "/:path*",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        },
-      ],
-    },
-  ],
 };
 
 module.exports = nextConfig;
