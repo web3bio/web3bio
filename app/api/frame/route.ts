5c480545490329ce9f5ec9018ed2f9e80345217d
billy <Billy@mask.io>
2024-02-07 14:47:56 +0800

    chore: back

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -3,18 +3,27 @@ import { NextRequest, NextResponse } from "next/server";
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL || "https://web3.bio";
 const profileEndpoint =
   process.env.NEXT_PUBLIC_PROFILE_END_POINT || "https://api.web3.bio";
+
+const defaultBack = () => NextResponse.redirect(baseURL, { status: 302 });
+
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const searchParams = req.nextUrl.searchParams;
   const defaultDomain = searchParams.get("domain");
+  let profiles = new Array();
 
-  if (!defaultDomain) return NextResponse.redirect(baseURL, { status: 302 });
-
+  if (!defaultDomain) return defaultBack();
   const data = await req.json();
-  const buttonId = data.untrustedData.buttonIndex;
-  const domain = data.untrustedData.url.split("/")[3];
-  const profiles = await fetch(`${profileEndpoint}/profile/${domain}`).then(
-    (res) => res.json()
-  );
+  const buttonId = data.untrustedData?.buttonIndex || 0;
+  const domain = data.untrustedData?.url?.split("/")?.[3] || defaultDomain;
+  try {
+    profiles = await fetch(`${profileEndpoint}/profile/${domain}`).then((res) =>
+      res.json()
+    );
+  } catch (e) {
+    console.error(e);
+    defaultBack();
+  }
+
   const redirectURL =
     baseURL + "/" + !profiles || buttonId === profiles?.length
       ? defaultDomain || ""
