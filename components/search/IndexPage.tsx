83f48e1de305a8a784e71e884c8fc705fff94944
Yan <picturepan2@hotmail.com>
2024-03-17 23:42:52 +0800

    Update

--- components/search/IndexPage.tsx
+++ components/search/IndexPage.tsx
@@ -7,6 +7,7 @@ import { handleSearchPlatform, isDomainSearch } from "../../utils/utils";
 import SearchResultDomain from "./SearchResultDomain";
 import SearchResultQuery from "./SearchResultQuery";
 import IndexPageListener from "./IndexPageListener";
+import { HomeFeatures } from "../shared/HomeFeatures";
 
 export default function HomePage() {
   const [searchFocus, setSearchFocus] = useState(false);
@@ -42,66 +43,69 @@ export default function HomePage() {
   }, [router, searchParams]);
 
   return (
-    <div className={searchFocus ? "web3bio-search focused" : "web3bio-search"}>
-      <div className="container grid-sm">
-        <div className="search-form">
-          <Link
-            href={{
-              pathname: "/",
-              query: {},
-            }}
-            className="web3bio-logo"
-            title="Web3.bio"
-          >
-            <h1 className="text-pride">
-              WEB3
-              <br />
-              BIO
-            </h1>
-            <h2 className="text-assistive">
-              Web3.bio is a platform for Web3 and Web 2.0 Identity Graph search
-              and link in bio profiles. It provides a list of relevant
-              identities when searching for a Twitter handle, Ethereum address,
-              ENS domain, Lens profile, Farcaster account, Unstoppable Domains,
-              and other Web3 identities.
-            </h2>
-          </Link>
-          <div className="form-label">
-            Web3 Identity Search
-            <br />
-            <small>Discover Web3 Identity Graph and Profiles</small>
-          </div>
-          <div className="form-input-group">
-            <SearchInput
-              // add key here to make defaultValue reactive
-              inputRef={inputRef}
-              key={searchTerm}
-              defaultValue={searchTerm}
-              handleSubmit={(value, platform) => {
-                handleSubmit(value, platform);
+    <>
+      <div className={searchFocus ? "web3bio-search focused" : "web3bio-search"}>
+        <div className="container grid-sm">
+          <div className="search-form">
+            <Link
+              href={{
+                pathname: "/",
+                query: {},
               }}
-            />
+              className="web3bio-logo"
+              title="Web3.bio"
+            >
+              <h1 className="text-pride">
+                WEB3
+                <br />
+                BIO
+              </h1>
+              <h2 className="text-assistive">
+                Web3.bio is a platform for Web3 and Web 2.0 Identity Graph search
+                and link in bio profiles. It provides a list of relevant
+                identities when searching for a Twitter handle, Ethereum address,
+                ENS domain, Lens profile, Farcaster account, Unstoppable Domains,
+                and other Web3 identities.
+              </h2>
+            </Link>
+            <div className="form-label">
+              Web3 Identity Search
+              <br />
+              <small>Explore Web3 identity and profiles in a whole new way</small>
+            </div>
+            <div className="form-input-group">
+              <SearchInput
+                // add key here to make defaultValue reactive
+                inputRef={inputRef}
+                key={searchTerm}
+                defaultValue={searchTerm}
+                handleSubmit={(value, platform) => {
+                  handleSubmit(value, platform);
+                }}
+              />
+            </div>
           </div>
+          {searchPlatform ? (
+            isDomainSearch(searchPlatform) ? (
+              <SearchResultDomain
+                searchTerm={searchTerm}
+                searchPlatform={searchPlatform}
+              />
+            ) : (
+              <SearchResultQuery
+                searchTerm={
+                  searchTerm.endsWith(".farcaster")
+                    ? searchTerm.replace(".farcaster", "")
+                    : searchTerm
+                }
+                searchPlatform={searchPlatform}
+              />
+            )
+          ) : null}
         </div>
-        {searchPlatform ? (
-          isDomainSearch(searchPlatform) ? (
-            <SearchResultDomain
-              searchTerm={searchTerm}
-              searchPlatform={searchPlatform}
-            />
-          ) : (
-            <SearchResultQuery
-              searchTerm={
-                searchTerm.endsWith(".farcaster")
-                  ? searchTerm.replace(".farcaster", "")
-                  : searchTerm
-              }
-              searchPlatform={searchPlatform}
-            />
-          )
-        ) : null}
+        <IndexPageListener inputRef={inputRef} />
       </div>
-      <IndexPageListener inputRef={inputRef} />
-    </div>
+      <HomeFeatures />
+    </>
   );
 }
