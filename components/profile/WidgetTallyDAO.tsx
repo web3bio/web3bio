0a80f408fd570c4ddef0c6dcadcbc5a317ec2c8f
billy <Billy@mask.io>
2024-04-07 17:03:30 +0800

    chore: tally dao inital tab state

--- components/profile/WidgetTallyDAO.tsx
+++ components/profile/WidgetTallyDAO.tsx
@@ -9,7 +9,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { Error } from "../shared/Error";
 import { Empty } from "../shared/Empty";
-import { NFTAssetPlayer } from "../shared/NFTAssetPlayer";
 import { formatText, formatBalance } from "../../utils/utils";
 
 const RenderWidgetTallyDAO = ({ address }) => {
@@ -44,10 +43,14 @@ const RenderWidgetTallyDAO = ({ address }) => {
     if (!loading) {
       dispatch(
         updateTallyDAOWidget({
-          isEmpty: !data?.delegates?.nodes?.length,
+          isEmpty:
+            !data?.delegates?.nodes?.length && !data?.delegatees?.nodes?.length,
           initLoading: false,
         })
       );
+    }
+    if (data && !data?.delegates?.nodes?.length) {
+      setActiveTab(1);
     }
   }, [data, loading, dispatch, activeTab]);
 
@@ -58,7 +61,6 @@ const RenderWidgetTallyDAO = ({ address }) => {
 
   const renderData =
     activeTab === 0 ? data?.delegates?.nodes : data?.delegatees?.nodes;
-  console.log(data?.delegatees?.nodes)
 
   return (
     <div className="profile-widget-full" id="DAO">
@@ -127,30 +129,39 @@ const RenderWidgetTallyDAO = ({ address }) => {
               <tbody>
                 {renderData.map((x, idx) => {
                   const votesCount = formatBalance(x.votesCount, 18, 2);
-                  return activeTab === 0 ? Number(x.organization?.delegatesVotesCount) !== 0 && (
-                    <tr key={"td" + idx}>
-                      <td>
-                        <div className="table-item">
-                          <Image
-                            className="dao-icon"
-                            src={x.organization.metadata?.icon || ""}
-                            height={24}
-                            width={24}
-                            alt={x.organization.name}
-                          />
-                          {x.organization.name}
-                        </div>
-                      </td>
-                      <td>
-                        {votesCount}{" "}
-                        ({(Math.abs(Number(x.votesCount) / Number(x.organization?.delegatesVotesCount)) * 100).toFixed(2) +"%"})
-                      </td>
-                      <td>
-                        <div className="badge">
-                          <strong>{x.delegatorsCount}</strong> addresses delegating
-                        </div>
-                      </td>
-                    </tr>
+                  return activeTab === 0 ? (
+                    Number(x.organization?.delegatesVotesCount) !== 0 && (
+                      <tr key={"td" + idx}>
+                        <td>
+                          <div className="table-item">
+                            <Image
+                              className="dao-icon"
+                              src={x.organization.metadata?.icon || ""}
+                              height={24}
+                              width={24}
+                              alt={x.organization.name}
+                            />
+                            {x.organization.name}
+                          </div>
+                        </td>
+                        <td>
+                          {votesCount} (
+                          {(
+                            Math.abs(
+                              Number(x.votesCount) /
+                                Number(x.organization?.delegatesVotesCount)
+                            ) * 100
+                          ).toFixed(2) + "%"}
+                          )
+                        </td>
+                        <td>
+                          <div className="badge">
+                            <strong>{x.delegatorsCount}</strong> addresses
+                            delegating
+                          </div>
+                        </td>
+                      </tr>
+                    )
                   ) : (
                     <tr key={"td" + idx}>
                       <td>
@@ -168,13 +179,22 @@ const RenderWidgetTallyDAO = ({ address }) => {
                           </div>
                         </div>
                       </td>
-                      <td>{formatBalance(x.votes, x.token.decimals, 2)} {x.token?.symbol}</td>
+                      <td>
+                        {formatBalance(x.votes, x.token.decimals, 2)}{" "}
+                        {x.token?.symbol}
+                      </td>
                       <td>
                         <div className="table-item">
                           {x.delegator.ens ? (
-                            <Link className="feed-token" href={`${
-                              process.env.NEXT_PUBLIC_BASE_URL || "https://web3.bio"
-                            }/${x.delegator.ens}`} title={x.delegator.name} target="_blank">
+                            <Link
+                              className="feed-token"
+                              href={`${
+                                process.env.NEXT_PUBLIC_BASE_URL ||
+                                "https://web3.bio"
+                              }/${x.delegator.ens}`}
+                              title={x.delegator.name}
+                              target="_blank"
+                            >
                               {x.delegator.picture && (
                                 <Image
                                   className="feed-token-icon"
@@ -188,10 +208,15 @@ const RenderWidgetTallyDAO = ({ address }) => {
                               <span className="feed-token-value">
                                 {x.delegator.name}
                               </span>
-                              <small className="feed-token-meta">{formatText(x.delegator?.address || "")}</small>
+                              <small className="feed-token-meta">
+                                {formatText(x.delegator?.address || "")}
+                              </small>
                             </Link>
                           ) : (
-                            <div className="feed-token" title={x.delegator.name}>
+                            <div
+                              className="feed-token"
+                              title={x.delegator.name}
+                            >
                               {x.delegator.picture && (
                                 <Image
                                   className="feed-token-icon"
@@ -205,7 +230,9 @@ const RenderWidgetTallyDAO = ({ address }) => {
                               <span className="feed-token-value">
                                 {x.delegator.name}
                               </span>
-                              <small className="feed-token-meta">{formatText(x.delegator?.address || "")}</small>
+                              <small className="feed-token-meta">
+                                {formatText(x.delegator?.address || "")}
+                              </small>
                             </div>
                           )}
                         </div>
