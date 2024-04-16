c16571ce9631ab08b5f4182f02b1601b897a982e
billy <Billy@mask.io>
2024-04-16 16:18:12 +0800

    chore: fix search modal conteng router ready redirect

--- components/modal/SearchModalContent.tsx
+++ components/modal/SearchModalContent.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import SearchInput from "../search/SearchInput";
@@ -6,32 +7,35 @@ export default function SearchModalContent(props) {
   const { domain, onClose } = props;
   const router = useRouter();
   return (
-    <div className="web3bio-search">
-      <Link
-        href={{
-          pathname: "/",
-          query: {},
-        }}
-        className="web3bio-logo"
-        title="Web3.bio"
-      >
-        <h1 className="text-pride">
-          WEB3
-          <br />
-          BIO
-        </h1>
-      </Link>
-      <div className="form-input-group">
-        <SearchInput
-          key={domain}
-          handleSubmit={(value, platform) => {
-            router.push(
-              `/?s=${value}${platform ? `&platform=${platform}` : ""}`
-            );
-            onClose();
+    router && (
+      <div className="web3bio-search">
+        <Link
+          href={{
+            pathname: "/",
+            query: {},
           }}
-        />
+          className="web3bio-logo"
+          title="Web3.bio"
+        >
+          <h1 className="text-pride">
+            WEB3
+            <br />
+            BIO
+          </h1>
+        </Link>
+        <div className="form-input-group">
+          <SearchInput
+            key={domain}
+            defaultValue={""}
+            handleSubmit={(value, platform) => {
+              router.push(
+                `/?s=${value}${platform ? `&platform=${platform}` : ""}`
+              );
+              onClose();
+            }}
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 }
