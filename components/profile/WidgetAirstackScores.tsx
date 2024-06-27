d75bf4f7f70c33600dbf4ad0c0eba3eb1a68cd98
billy <Billy@mask.io>
2024-06-27 17:20:13 +0800

    chore: update

--- components/profile/WidgetAirstackScores.tsx
+++ components/profile/WidgetAirstackScores.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { WidgetInfoMapping, WidgetTypes } from "../utils/widgets";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { updateAirstackScoresWidget } from "../state/widgets/action";
 import { useQuery } from "@apollo/client";
@@ -27,41 +27,44 @@ export function WidgetAirStackScores({ handle }) {
     }
   }, [data, dispatch, loading]);
 
-  const socialCapital = data?.Socials?.Social?.[0]?.socialCapital;
+  const socialCapital = useMemo(
+    () => data?.Socials?.Social?.[0]?.socialCapital,
+    [data]
+  );
 
-  if (!loading && !socialCapital) return null;
-  return loading ? (
-    <></>
-  ) : (
-    <div className="profile-widget profile-widget-webacy">
-      <div className="profile-widget-header">
-        <h2 className="profile-widget-title">
-          <span className="emoji-large mr-2">
-            {WidgetInfoMapping(WidgetTypes.airstackScores).icon}{" "}
-          </span>
-          {WidgetInfoMapping(WidgetTypes.airstackScores).title}{" "}
-        </h2>
-      </div>
-
-      <div className="profile-widget-body"></div>
-
-      <div className="profile-widget-footer">
-        <div className="widget-risk-number">
-          {Number(socialCapital?.socialCapitalScore).toFixed(2)}
-          <div
-            className={`widget-risk-label ${
-              socialCapital?.socialCapitalRank < 10
-                ? "high-risk"
-                : socialCapital?.socialCapitalRank < 100
-                ? "medium-risk"
-                : "low-risk"
-            }`}
-          >
-            Rank: {socialCapital?.socialCapitalRank}
-          </div>
+  if ((!loading && !socialCapital) || error) return null;
+  return (
+    socialCapital && (
+      <div className="profile-widget profile-widget-webacy">
+        <div className="profile-widget-header">
+          <h2 className="profile-widget-title">
+            <span className="emoji-large mr-2">
+              {WidgetInfoMapping(WidgetTypes.airstackScores).icon}{" "}
+            </span>
+            {WidgetInfoMapping(WidgetTypes.airstackScores).title}{" "}
+          </h2>
         </div>
-        <div className="widget-risk-title">Social Capital Scores</div>
+
+        <div className="profile-widget-body"></div>
+
+        <div className="profile-widget-footer">
+          <div className="widget-risk-number">
+            {Number(socialCapital?.socialCapitalScore).toFixed(2)}
+            <div
+              className={`widget-risk-label ${
+                socialCapital?.socialCapitalRank < 10
+                  ? "high-risk"
+                  : socialCapital?.socialCapitalRank < 100
+                  ? "medium-risk"
+                  : "low-risk"
+              }`}
+            >
+              Rank: {socialCapital?.socialCapitalRank}
+            </div>
+          </div>
+          <div className="widget-risk-title">Social Capital Scores</div>
+        </div>
       </div>
-    </div>
+    )
   );
 }
