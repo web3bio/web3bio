a9a74c235a5813101a478252e99ca1a2422f4290
billy <Billy@mask.io>
2024-09-10 13:01:02 +0800

    chore: custom list

--- components/shared/WalletProvider.tsx
+++ components/shared/WalletProvider.tsx
@@ -4,11 +4,40 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider, http } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import {
+  injectedWallet,
+  rainbowWallet,
+  metaMaskWallet,
+  coinbaseWallet,
+  walletConnectWallet,
+  zerionWallet,
+  okxWallet,
+  imTokenWallet,
+  binanceWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 
 export const config = getDefaultConfig({
   appName: "Web3.bio",
   projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_ID || "",
   chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+  wallets: [
+    {
+      groupName: "Injected",
+      wallets: [injectedWallet, metaMaskWallet],
+    },
+    {
+      groupName: "Recommend",
+      wallets: [
+        zerionWallet,
+        okxWallet,
+        coinbaseWallet,
+        rainbowWallet,
+        imTokenWallet,
+        binanceWallet,
+        walletConnectWallet,
+      ],
+    },
+  ],
   transports: {
     [mainnet.id]: http(),
   },
